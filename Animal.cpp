857ee6d74c315f11b256267fc13c09a8ec0f4a73
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-05-29 14:16:26 +0700

    ws

---
+++ Animal.cpp
@@ -0,0 +1,15 @@
+#include "Animal.h"
+
+Animal::Animal(string pName, string pSpecies)
+{
+    name = pName;
+    species = pSpecies;
+}
+
+void Animal::eat() {
+    cout << name << "the " << species << " eat some generic food" << endl;
+}
+
+void Animal::sleep() {
+    cout << "The generic animal sleeps...generically?" << endl;
+}
