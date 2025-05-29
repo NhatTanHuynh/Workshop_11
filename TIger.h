838d301569b04ec1885233f3008703fa6278c733
NhatTanHuynh <a1948706@adelaide.edu.au>
2025-05-29 14:37:51 +0700

    hh

--- TIger.h
+++ TIger.h
@@ -1,13 +1,15 @@
 #ifndef TIGER_H
 #define TIGER_H
 #include "Animal.h"
+
 class Tiger : public Animal
 {
-int kills;
+    int kills;
 public:
-Tiger(string pName);
-void hunt();
-virtual void eat();
-virtual void sleep();
+    Tiger(string pName);
+    void hunt();
+    virtual void eat();
+    virtual void sleep();
 };
+
 #endif
